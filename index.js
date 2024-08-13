d23613f4cb5e201a56ba022693ff0df5fcfcceca
Rafa <wizd_x@hotmail.com>
2024-08-12 23:26:51 -0300

    Beta Version

---
+++ index.js
@@ -0,0 +1,19 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+
+function RollRight(){
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex; margin-top:55px"
+}
+
+function RollLeft(){
+    leonardo.style = "display:flex"
+    bruna.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:flex; margin-top:55px"
+}
